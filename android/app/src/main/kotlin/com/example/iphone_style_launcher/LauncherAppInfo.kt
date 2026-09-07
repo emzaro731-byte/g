0963caec6206cddd5ad951c55710065b76ccd02a
emzaro731-byte <emzaro731@gmail.com>
2026-09-07 18:08:32 +0100

    Add launcher app model

---
+++ android/app/src/main/kotlin/com/example/iphone_style_launcher/LauncherAppInfo.kt
@@ -0,0 +1,8 @@
+package com.example.iphone_style_launcher
+
+data class LauncherAppInfo(
+    val name: String,
+    val packageName: String,
+    val className: String,
+    val iconBase64: String
+)
